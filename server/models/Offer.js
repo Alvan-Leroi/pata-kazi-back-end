be3eb1741d3e969e220b30531bf12669b60d5cb6
Alvan Leroi <rmafumu@gmail.com>
2026-08-27 20:49:37 +0300

    Fix provider offer backend

--- server/models/Offer.js
+++ server/models/Offer.js
@@ -1,59 +1,57 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+const mongoose = require("mongoose");
 
-import Login from "./login/login";
-import Signup from "./signup/signup";
+const offerSchema = new mongoose.Schema(
+  {
+    taskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
 
-import Home from "./home/home";
-import Account from "./account/account";
+    providerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
 
-import Terms from "./terms/terms";
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
 
-import PostTask from "./tasks/PostTask";
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
 
-import MatchingProviders from "./tasks/MatchingProviders";
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 1000,
+    },
 
-import ProviderHome from "./provider/ProviderHome";
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "declined", "withdrawn"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-import ProviderJob from "./provider/ProviderJob";
+offerSchema.index(
+  {
+    taskId: 1,
+    providerId: 1,
+  },
+  {
+    unique: true,
+  },
+);
 
-import ProviderAccount from "./provider/ProviderAccount";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* LOGIN */}
-        <Route path="/" element={<Login />} />
-
-        {/* SIGNUP */}
-        <Route path="/signup" element={<Signup />} />
-
-        {/* CUSTOMER HOME */}
-        <Route path="/home" element={<Home />} />
-
-        {/* CUSTOMER ACCOUNT */}
-        <Route path="/account" element={<Account />} />
-
-        {/* TERMS */}
-        <Route path="/terms" element={<Terms />} />
-
-        {/* CUSTOMER POSTS TASK */}
-        <Route path="/post-task" element={<PostTask />} />
-
-        {/* CUSTOMER PROVIDER MATCHES */}
-        <Route path="/task/:taskId/providers" element={<MatchingProviders />} />
-
-        {/* PROVIDER HOME */}
-        <Route path="/provider" element={<ProviderHome />} />
-
-        {/* PROVIDER ACCOUNT */}
-        <Route path="/provider-account" element={<ProviderAccount />} />
-
-        {/* PROVIDER JOB */}
-        <Route path="/provider/job/:jobId" element={<ProviderJob />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+module.exports = mongoose.model("Offer", offerSchema);
